c3cc4b22e96a4d47252191ade1808bb9059329d4
GrayCoded <130934177+GrayCoded@users.noreply.github.com>
2023-09-25 12:24:19 -0300

    made some changes to the install event listeners

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,4 +18,20 @@ export const putDb = async (content) => console.error('putDb not implemented');
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
+const jateDb = await openDB('jate', 1);
+
+// Create a new transaction and specify the database and data privileges.
+const tx = jateDb.transaction('jate', 'readonly');
+
+// Open up the desired object store.
+const store = tx.objectStore('jate');
+
+// Use the .get() method to get a piece of data from the database based on the id.
+const request = store.get(id);
+
+// Get confirmation of the request.
+const result = await request;
+console.log('result.value', result);
+return result;
+
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,31 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('beforeinstallprompt', event);
+    event.preventDefault();
+    installPrompt = event;
+    butInstall.style.display = 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (installPrompt) {
+        installPrompt.prompt();
+
+        installPrompt.userSelect.then((selected) => {
+            if (selected.outcome === 'accepted') {
+                console.log('Installation prompt accepted');
+            } else {
+                console.log('Installation prompt dismissed');
+            }
+            installPrompt = null;
+            butInstall = 'none';
+        });
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('Application Installed!', event);
+});
